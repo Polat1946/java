43283b71cfd95639960ea6a0ac3e71b5336e4b23
Polat1946 <139751472+Polat1946@users.noreply.github.com>
2023-08-02 18:31:00 +0100

    Add files via upload

---
+++ 30days/helloworld.js
@@ -0,0 +1,40 @@
+console.log('zaman hanci bulut yolcu')
+let victory = ["amed", "hewler", "mahabad"];
+
+victory.push("azadi");
+
+console.log(victory);
+
+let myAlphabet = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+    "$",
+    "*",
+    "/",
+    "-",
+    "+",
+  ];
---
+++ 30days/introduction.js
@@ -0,0 +1,22 @@
+console.log('bu yazi jv den geldi!')
+console.log("an azadi an neman")
+console.log(typeof probelg)
+console.log(typeof 5)
+console.log(typeof true)
+console.log(typeof null)
+console.log(typeof undefined)
+let firstName = 'ihsanNuri'
+let lastName = 'pasa'
+let country = ' Belgium' 
+let age = '1946' 
+
+const GRAVITY = 9.81
+const PI = 3.14
+
+console.log(firstName,lastName,country,age,GRAVITY,PI)
+
+let firsName = 'ihsanNuri46',
+     lasName = 'pasa1',
+    coutry = ' Belgium12', 
+    ae = '1946m' 
+    console.log(firsName,lasName,coutry,ae)
